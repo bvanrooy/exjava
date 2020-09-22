ce6744a98df4231b5af92364676b84519384f5e6
bvanrooy <bart.van.rooy@gmail.com>
2020-09-22 14:01:41 +0200

    exercise C01

---
+++ exc01/ExC01.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner; 
+
+public class ExC01{
+	
+	public static void main(String[] args){
+		  
+		String title;
+		int numberOfDays;
+		double pricePerDay;
+		boolean priorKnowledgeNeeded;
+		ArrayList<String> instructors = new ArrayList<>();
+        double totalPriceVatIncl;
+        final double VATPERCENTAGE = 21;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter course title : ");
+		title=scanner.nextLine();
+		System.out.println("Enter number of days : ");
+		numberOfDays = Integer.parseInt(scanner.nextLine());
+		System.out.println("Enter price per day : ");
+		pricePerDay = Double.parseDouble(scanner.nextLine());
+
+		priorKnowledgeNeeded = true;
+
+		instructors.add("Sandy Schillebeeckx");
+		instructors.add("Luigi Instructore");
+
+		totalPriceVatIncl = pricePerDay * numberOfDays ;
+		if(!(numberOfDays > 3 && priorKnowledgeNeeded)){
+			totalPriceVatIncl = totalPriceVatIncl + ( totalPriceVatIncl * VATPERCENTAGE / 100);
+		}		
+		
+		printInfo(title,numberOfDays,pricePerDay,totalPriceVatIncl,priorKnowledgeNeeded,instructors);
+
+	
+	}
+	
+	public static void printInfo(String title, int numberOfDays, double pricePerDay, double totalPriceVatIncl, boolean priorKnowledgeNeeded,ArrayList<String> instructors){
+		System.out.println("Course\n-----------------------------------");
+        System.out.println("Title :  " + title);
+        System.out.println("Number of days : " + numberOfDays);
+        System.out.println("Price / day : " + pricePerDay);
+		System.out.println("Total Price VAT Incl : " + totalPriceVatIncl);
+		System.out.println(getPriceCategory(totalPriceVatIncl));
+        System.out.println("Prior knowledge needed : " + priorKnowledgeNeeded);
+        System.out.println("Number of instructors : " + instructors.size());
+        for (String instructorForCourse : instructors) {
+            System.out.println("\t\tInstructor : " + instructorForCourse);
+        }		
+		System.out.println("\n-----------------------------------------------------------");
+
+	}
+	
+	private static String getPriceCategory(double price){
+		if(price< 500){
+			return "CHEAP";
+		}
+		else if(price > 499 && price < 1500){
+			return "OK";
+		}else{
+			return "EXPENSIVE";
+		}
+	}
+		
+}
