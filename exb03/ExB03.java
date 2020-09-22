35a398a814bf85fe380c59750b92562150433c13
bvanrooy <bart.van.rooy@gmail.com>
2020-09-22 11:34:48 +0200

    exercise b03

---
+++ exb03/ExB03.java
@@ -0,0 +1,17 @@
+public class ExB03{
+
+	public static void main(String[] args){
+		int[] integerArray = {1,2,3,4,5,6,7,8,9,10};
+		double total=0;
+		double average;
+		
+		for(int i=0;i<integerArray.length;i++){
+				total = total+integerArray[i];
+		}
+		
+		average=total/integerArray.length;
+		
+		System.out.println("Total : " + total + " average : " + average);
+		
+	}
+}
