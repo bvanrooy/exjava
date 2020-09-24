40e984314ae87a930435898455d6230fc5754b06
bvanrooy <bart.van.rooy@gmail.com>
2020-09-24 09:23:13 +0200

    exercise G03

---
+++ exg03/AgeCannotBeNegativeException.java
@@ -0,0 +1,18 @@
+public class AgeCannotBeNegativeException extends Exception {
+
+    public AgeCannotBeNegativeException(){
+        super();
+    }
+
+    public AgeCannotBeNegativeException(String message){
+        super(message);
+    }
+
+    public AgeCannotBeNegativeException(Throwable throwable){
+        super(throwable);
+    }
+
+    public AgeCannotBeNegativeException(String message, Throwable throwable){
+        super(message,throwable);
+    }
+}
